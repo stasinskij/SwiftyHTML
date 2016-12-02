df1a776615c1d22c770c865bbdaf64aee2630b87
Bartosz Tułodziecki <bartosz.tulodziecki@implix.com>
2016-12-02 10:26:38 +0100

    Cleanup.

---
+++ Example/SwiftyHTML/TestHTML.swift
@@ -0,0 +1,37 @@
+//
+//  TestHTML.swift
+//  SwiftyHTML
+//
+//  Created by Bartosz Tułodziecki on 02/12/2016.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class TestHtml {
+    
+    static var multipleAttributesStringWithLink: NSMutableAttributedString = {
+        let prefix = "A "
+        let urlString = "Google link"
+        let suffix = " string"
+        let inputString = NSString(string: prefix + urlString + suffix)
+        
+        let str = NSMutableAttributedString(string: inputString as String)
+        
+        
+        str.addAttribute(NSLinkAttributeName, value: NSURL.init(string: "https://www.google.com")!, range: inputString.range(of: urlString, options: .caseInsensitive))
+        str.addAttribute(NSForegroundColorAttributeName, value: UIColor.red, range: NSMakeRange(0, 4))
+        str.addAttribute(NSFontAttributeName, value: UIFont.boldSystemFont(ofSize: 16), range: NSMakeRange(0, 4))
+        str.addAttribute(NSFontAttributeName, value: UIFont.boldSystemFont(ofSize: 24), range: inputString.range(of: suffix, options: .caseInsensitive))
+        str.addAttribute(NSBackgroundColorAttributeName, value: UIColor.green, range: inputString.range(of: suffix, options: .caseInsensitive))
+        
+        let palatinoString = NSMutableAttributedString(string: "\nthis is Palatino-Roman string")
+        palatinoString.addAttribute(NSFontAttributeName, value: UIFont.init(name: "Palatino-Roman", size: 16.0)!, range: NSMakeRange(0, palatinoString.length))
+        
+        str.append(palatinoString)
+        
+        let cleanString = NSMutableAttributedString(string: "\nthis is a string with no attributes")
+        str.append(cleanString)
+        return str
+    }()
+}
--- Example/SwiftyHTML/ViewController.swift
+++ Example/SwiftyHTML/ViewController.swift
@@ -10,66 +10,20 @@ import UIKit
 import SwiftyHTML
 import Foundation
 
-class ViewController: UIViewController, UITextViewDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var webView: UIWebView!
     
-    private lazy var linkString: NSMutableAttributedString = {
-        let prefix = "A "
-        let urlString = "Google link"
-        let suffix = " string"
-        let inputString = NSString(string: prefix + urlString + suffix)
-        
-        let str = NSMutableAttributedString(string: inputString as String)
-        
-        
-        str.addAttribute(NSLinkAttributeName, value: NSURL.init(string: "https://www.google.com")!, range: inputString.range(of: urlString, options: .caseInsensitive))
-//        str.addAttribute(NSForegroundColorAttributeName, value: UIColor.red, range: inputString.range(of: prefix, options: .caseInsensitive))
-        str.addAttribute(NSForegroundColorAttributeName, value: UIColor.red, range: NSMakeRange(0, 4))
-        str.addAttribute(NSFontAttributeName, value: UIFont.boldSystemFont(ofSize: 16), range: NSMakeRange(0, 4))
-        str.addAttribute(NSFontAttributeName, value: UIFont.boldSystemFont(ofSize: 24), range: inputString.range(of: suffix, options: .caseInsensitive))
-        str.addAttribute(NSBackgroundColorAttributeName, value: UIColor.green, range: inputString.range(of: suffix, options: .caseInsensitive))
-        
-        let palatinoString = NSMutableAttributedString(string: "\nthis is Palatino-Roman string")
-        palatinoString.addAttribute(NSFontAttributeName, value: UIFont.init(name: "Palatino-Roman", size: 16.0)!, range: NSMakeRange(0, palatinoString.length))
-        
-        str.append(palatinoString)
-        
-        let cleanString = NSMutableAttributedString(string: "\nthis is a string with no attributes")
-        str.append(cleanString)
-        return str
-    }()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.textView.attributedText = self.linkString
-        
-        self.linkString.enumerateAttributes(in: NSMakeRange(0, self.linkString.length), options: .longestEffectiveRangeNotRequired) { (attributes, range, stop) in
-            for (_, attribute) in attributes {
-                if attribute is UIFont {
-                    print((attribute as! UIFont).familyName)
-                    print((attribute as! UIFont).fontName)
-//                    print((attribute as! UIFont).fontDescriptor)
-//                    print((attribute as! UIFont).fontDescriptor.object(forKey: UIFontDescriptorTraitsAttribute))
-                    print("\(attribute) is bold? \((attribute as! UIFont).isBold)")
-                    print("\(attribute) is italic? \((attribute as! UIFont).isItalic)")
-                }
-            }
-        }
+        self.textView.attributedText = TestHtml.multipleAttributesStringWithLink
         
         let defaultStyle = Style.init(value: "font-family:apple-system, sans-serif; font-size:14px")
-        let htmlString = self.linkString.toHTML(defaultStyle: defaultStyle)
+        let htmlString = TestHtml.multipleAttributesStringWithLink.toHTML(defaultStyle: defaultStyle)
         self.webView.loadHTMLString(htmlString, baseURL: nil)
         print("html string: \(htmlString)")
-    }
-    
-    // MARK: TextView Delegate methods
-    
-    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange) -> Bool {
-        UIApplication.shared.openURL(URL)
-        return false
     }
 }
 
